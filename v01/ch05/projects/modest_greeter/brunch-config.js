ff0322b8e8f020e1ff19857141b52d0cdfbccecd
INABA Akira <inaba.akira@gmail.com>
2018-05-14 12:57:36 +0900

    第11章終了

--- v01/ch05/projects/modest_greeter/brunch-config.js
+++ v01/ch05/projects/modest_greeter/brunch-config.js
@@ -54,7 +54,10 @@ exports.config = {
     babel: {
       // Do not use ES6 compiler in vendor code
       ignore: [/web\/static\/vendor/]
-    }
+    },
+      copycat: {
+          fonts: ["node_modules/font-awesome/fonts"]
+      }
   },
 
   modules: {
